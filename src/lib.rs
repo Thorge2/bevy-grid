f3fedf2e7c21c26e3d3ed0c849a01c7881639fee
Thorge Hemken <thorge@flo.rip>
2021-02-27 20:14:43 +0100

    added fit modes and refactor

---
+++ src/lib.rs
@@ -0,0 +1,121 @@
+use bevy::prelude::*;
+
+pub struct GridPlugin;
+impl Plugin for GridPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_resource(Grid::default());
+        app.add_system(update_grid.system());
+    }
+}
+
+#[derive(Bundle)]
+pub struct GridBundle {
+    pub position: Position,
+    pub size: Size,
+}
+impl Default for GridBundle {
+    fn default() -> Self {
+        GridBundle {
+            position: Position::default(),
+            size: Size::default(),
+        }
+    }
+}
+
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+impl Position {
+    #[allow(dead_code)]
+    pub fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+impl Default for Position {
+    fn default() -> Self {
+        Position { x: 0, y: 0 }
+    }
+}
+
+pub struct Size {
+    pub width: u32,
+    pub height: u32,
+}
+impl Size {
+    #[allow(dead_code)]
+    pub fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
+    }
+}
+impl Default for Size {
+    fn default() -> Self {
+        Size {
+            width: 1,
+            height: 1,
+        }
+    }
+}
+
+pub struct Grid {
+    pub width: u32,
+    pub height: u32,
+    pub mode: GridMode,
+}
+impl Default for Grid {
+    fn default() -> Self {
+        Grid {
+            width: 10,
+            height: 10,
+            mode: GridMode::Fit,
+        }
+    }
+}
+
+pub enum GridMode {
+    Fit,
+    FitX,
+    FitY,
+}
+
+fn update_grid(
+    mut grid_query: Query<(&Position, &Size, &mut Transform, &mut Sprite)>,
+    grid: Res<Grid>,
+    windows: Res<Windows>,
+) {
+    let window = windows.get_primary().unwrap();
+    for (position, size, mut transform, mut sprite) in grid_query.iter_mut() {
+        let step_x = window.width() / grid.width as f32;
+        let step_y = window.height() / grid.height as f32;
+
+        let width = step_x * size.width as f32;
+        let height = step_y * size.height as f32;
+
+        match grid.mode {
+            GridMode::Fit => {
+                sprite.size = Vec2::new(width, height);
+                transform.translation.x =
+                    step_x * position.x as f32 - window.width() / 2.0 + width / 2.0;
+                transform.translation.y =
+                    -step_y * position.y as f32 + window.height() / 2.0 - height / 2.0;
+            }
+
+            GridMode::FitY => {
+                sprite.size = Vec2::new(height, height);
+                transform.translation.x =
+                    step_y * position.x as f32 - window.width() / 2.0 + height / 2.0;
+                transform.translation.y =
+                    -step_y * position.y as f32 + window.height() / 2.0 - height / 2.0;
+            }
+
+            GridMode::FitX => {
+                sprite.size = Vec2::new(width, width);
+
+                transform.translation.x =
+                    step_x * position.x as f32 - window.width() / 2.0 + width / 2.0;
+                transform.translation.y =
+                    -step_x * position.y as f32 + window.height() / 2.0 - width / 2.0;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use bevy::prelude::*;
+use grid::*;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(GridPlugin)
+        .add_resource(Grid::default())
         .add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_startup_system(setup.system())
         .add_system(move_player.system())
         .add_system(change_grid.system())
+        .add_system(change_mode.system())
         .run();
 }
 
@@ -20,18 +23,28 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
             ..Default::default()
         })
         .with_bundle(GridBundle {
-            size: GridSize::new(2, 2),
+            size: grid::Size::new(1, 4),
             ..Default::default()
+        });
+
+    commands
+        .spawn(SpriteBundle {
+            material: materials.add(Color::rgb(0.9, 0.9, 0.9).into()),
+            ..Default::default()
+        })
+        .with_bundle(GridBundle {
+            size: grid::Size::new(2, 1),
+            position: Position::new(1, 3),
         });
 }
 
-fn move_player(mut player_query: Query<&mut GridPosition>, keyboard_input: Res<Input<KeyCode>>) {
+fn move_player(mut player_query: Query<&mut Position>, keyboard_input: Res<Input<KeyCode>>) {
     for mut position in player_query.iter_mut() {
         if keyboard_input.just_pressed(KeyCode::S) {
-            position.y -= 1;
+            position.y += 1;
         }
         if keyboard_input.just_pressed(KeyCode::W) {
-            position.y += 1;
+            position.y -= 1;
         }
         if keyboard_input.just_pressed(KeyCode::A) {
             position.x -= 1;
@@ -54,92 +67,16 @@ fn change_grid(mut grid: ResMut<Grid>, keyboard_input: Res<Input<KeyCode>>) {
     }
 }
 
-struct GridPlugin;
-impl Plugin for GridPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(Grid::default());
-        app.add_system(update_grid.system());
+fn change_mode(mut grid: ResMut<Grid>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.just_pressed(KeyCode::F) {
+        grid.mode = GridMode::Fit;
     }
-}
 
-#[derive(Bundle)]
-struct GridBundle {
-    pub position: GridPosition,
-    pub size: GridSize,
-}
-impl Default for GridBundle {
-    fn default() -> Self {
-        GridBundle {
-            position: GridPosition::default(),
-            size: GridSize::default(),
-        }
+    if keyboard_input.just_pressed(KeyCode::X) {
+        grid.mode = GridMode::FitX;
     }
-}
 
-struct GridPosition {
-    x: i32,
-    y: i32,
-}
-impl GridPosition {
-    #[allow(dead_code)]
-    fn new(x: i32, y: i32) -> Self {
-        Self { x, y }
-    }
-}
-impl Default for GridPosition {
-    fn default() -> Self {
-        GridPosition { x: 0, y: 0 }
-    }
-}
-
-struct GridSize {
-    width: u32,
-    height: u32,
-}
-impl GridSize {
-    #[allow(dead_code)]
-    fn new(width: u32, height: u32) -> Self {
-        Self { width, height }
-    }
-}
-impl Default for GridSize {
-    fn default() -> Self {
-        GridSize {
-            width: 1,
-            height: 1,
-        }
-    }
-}
-
-struct Grid {
-    width: u32,
-    height: u32,
-}
-impl Default for Grid {
-    fn default() -> Self {
-        Grid {
-            width: 10,
-            height: 10,
-        }
-    }
-}
-
-fn update_grid(
-    mut grid_query: Query<(&GridPosition, &GridSize, &mut Transform, &mut Sprite)>,
-    grid: Res<Grid>,
-    windows: Res<Windows>,
-) {
-    let window = windows.get_primary().unwrap();
-    for (position, size, mut transform, mut sprite) in grid_query.iter_mut() {
-        let step_x = window.width() / grid.width as f32;
-        let step_y = window.height() / grid.height as f32;
-
-        let width = step_x * size.width as f32;
-        let height = step_y * size.height as f32;
-
-        sprite.size = Vec2::new(width, height);
-
-        transform.translation.x = step_x * position.x as f32 - window.width() / 2.0 + width / 2.0;
-        transform.translation.y = step_y * position.y as f32 - window.height() / 2.0 + height / 2.0;
+    if keyboard_input.just_pressed(KeyCode::Y) {
+        grid.mode = GridMode::FitY;
     }
 }
